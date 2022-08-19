d98c0086239a1eda4402fd4412362059aea90362
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-08-19 14:22:17 +0300

    guardian-implementation save otps

--- providers/totp.go
+++ providers/totp.go
@@ -2,24 +2,43 @@ package providers
 
 import (
 	"crypto"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"sync"
 
 	"github.com/sec51/twofactor"
 )
 
 type totp struct {
-	issuer string
-	digits int
-	otps   map[string]*twofactor.Totp //TODO: fork and use twofactor in order to change consts like backoff_minutes
+	issuer      string
+	digits      int
+	otps        map[string]*twofactor.Totp //TODO: fork and use twofactor in order to change consts like backoff_minutes
+	otpsEncoded map[string][]byte
 	sync.RWMutex
 }
 
 func NewTOTP(issuer string, digits int) (*totp, error) {
+	otpsEncoded, err := readOtps("otpsEncoded")
+	if otpsEncoded == nil {
+		otpsEncoded = make(map[string][]byte)
+	}
+	otps := make(map[string]*twofactor.Totp)
+
+	for address, otp := range otpsEncoded {
+		otps[address], err = twofactor.TOTPFromBytes(otp, issuer)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &totp{
-		issuer: issuer,
-		digits: digits,
-		otps:   make(map[string]*twofactor.Totp),
+		issuer:      issuer,
+		digits:      digits,
+		otps:        otps,
+		otpsEncoded: otpsEncoded,
 	}, nil
 
 }
@@ -30,8 +49,37 @@ func (p *totp) Validate(account, usercode string) (bool, error) {
 		return false, fmt.Errorf("no otp created for account: %s", account)
 	}
 	err := otp.Validate(usercode)
+	if err != nil {
+		return false, err
+	}
+
+	err = p.update(account, otp)
+	if err != nil {
+		return false, err
+	}
 	isValid := err == nil
 	return isValid, err
+}
+
+func (p *totp) update(account string, otp *twofactor.Totp) error {
+	p.Lock()
+	defer p.Unlock()
+	otpBytes, err := otp.ToBytes()
+	if err != nil {
+		return nil
+	}
+	oldOtpEncoded, exists := p.otpsEncoded[account]
+	p.otpsEncoded[account] = otpBytes
+	err = saveOtp("otpsEncoded", p.otpsEncoded)
+	if err != nil {
+		if exists {
+			p.otpsEncoded[account] = oldOtpEncoded
+		}
+		return err
+	}
+	p.otps[account] = otp
+
+	return nil
 }
 
 func (p *totp) RegisterUser(account string) ([]byte, error) {
@@ -45,9 +93,43 @@ func (p *totp) RegisterUser(account string) ([]byte, error) {
 		return nil, err
 	}
 
-	p.Lock()
-	p.otps[account] = otp
-	p.Unlock()
+	err = p.update(account, otp)
+	if err != nil {
+		return nil, err
+	}
 
 	return qrBytes, nil
+}
+
+// Read the key file into a 32 byte array
+func readOtps(filename string) (map[string][]byte, error) {
+	// read the data back
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s.json", filename))
+	if err != nil {
+		return nil, err
+	}
+	var otpsEncoded map[string][]byte
+	err = json.Unmarshal(data, &otpsEncoded)
+
+	return otpsEncoded, err
+}
+
+// Write the key file hex encoded
+func saveOtp(filename string, otps map[string][]byte) error {
+	filePath := fmt.Sprintf("%s.json", filename)
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0777)
+	defer file.Close()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	jsonOtps, err := json.Marshal(otps)
+
+	_, err = file.Write(jsonOtps)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
