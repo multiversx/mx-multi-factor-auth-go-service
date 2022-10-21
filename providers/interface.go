cdfe84edc8ef10fa747ed97d5bcae76b549be185
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2022-10-21 17:43:52 +0300

    added missing file

--- providers/interface.go
+++ providers/interface.go
@@ -1,6 +1,8 @@
 package providers
 
-type OTPHandler interface {
-	SaveOTP(account, guardianAddr string)
-	GetOTP(account, guardianAddr string)
+// Provider defines the actions needed to be performed by a multi-auth provider
+type Provider interface {
+	ValidateCode(account, guardian, userCode string) error
+	RegisterUser(account, guardian string) ([]byte, error)
+	IsInterfaceNil() bool
 }
