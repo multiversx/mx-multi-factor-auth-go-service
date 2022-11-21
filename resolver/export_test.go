b05147929ee7957c559d72feaabf6b05c7950999
AdoAdoAdo <anaridado@gmail.com>
2022-11-21 17:05:37 +0200

    remove export_test file.

--- resolver/export_test.go
+++
@@ -1,8 +0,0 @@
-package resolver
-
-import erdCore "github.com/ElrondNetwork/elrond-sdk-erdgo/core"
-
-// GetGuardianAddress -
-func (resolver *serviceResolver) GetGuardianAddress(userAddress erdCore.AddressHandler) (string, error) {
-	return resolver.getGuardianAddress(userAddress)
-}
