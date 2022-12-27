a4573226e42aa908f5f1c1e045c3cbcd0246c5c6
AdoAdoAdo <anaridado@gmail.com>
2022-12-27 12:11:01 +0200

    add proto schemas

--- core/data.go
+++ core/data.go
@@ -1,14 +1,6 @@
+//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/ElrondNetwork/protobuf/protobuf  --gogoslick_out=. userInfo.proto
+
 package core
-
-// GuardianState represents the state of the guardian
-type GuardianState uint32
-
-const (
-	// NotUsableYet represents a guardian recently generated but not verified
-	NotUsableYet GuardianState = iota
-	// Usable represents a verified guardian
-	Usable
-)
 
 // OnChainGuardianState represents the on chain state of the guardian
 type OnChainGuardianState uint32
@@ -21,17 +13,3 @@ const (
 	// MissingGuardian represents a guardian missing from chain
 	MissingGuardian
 )
-
-// GuardianInfo holds details about a guardian
-type GuardianInfo struct {
-	PublicKey  []byte
-	PrivateKey []byte
-	State      GuardianState
-}
-
-// UserInfo holds info about both user's guardians and its unique index
-type UserInfo struct {
-	Index          uint32
-	FirstGuardian  GuardianInfo
-	SecondGuardian GuardianInfo
-}
