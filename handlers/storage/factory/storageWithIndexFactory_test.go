fc96931841932fa947a39de10078283dfbebbc09
ssd04 <dariussuirab@gmail.com>
2023-04-12 14:06:35 +0300

    add test in factory for collection ids mappings

--- handlers/storage/factory/storageWithIndexFactory_test.go
+++ handlers/storage/factory/storageWithIndexFactory_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/multiversx/multi-factor-auth-go-service/core"
 	"github.com/multiversx/multi-factor-auth-go-service/handlers"
 	"github.com/multiversx/multi-factor-auth-go-service/handlers/storage"
+	"github.com/multiversx/multi-factor-auth-go-service/mongodb"
 	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-storage-go/storageUnit"
 	"github.com/stretchr/testify/assert"
@@ -210,6 +211,77 @@ func TestNewShardedStorageFactory_Create(t *testing.T) {
 		_, err = shardedStorageInstance.Get([]byte("key"))
 		assert.Equal(t, storage.ErrKeyNotFound, err)
 	})
+}
+
+func TestMongoCollectionIDs(t *testing.T) {
+	t.Parallel()
+
+	t.Run("real storage MongoDB, should map buckets with collections correctly", func(t *testing.T) {
+		t.Parallel()
+
+		if os.Getenv("CI") != "" {
+			t.Skip("Skipping testing in CI environment")
+		}
+
+		inMemoryMongoDB, err := memongo.StartWithOptions(&memongo.Options{MongoVersion: "4.4.0", ShouldUseReplica: true})
+		require.Nil(t, err)
+		defer inMemoryMongoDB.Stop()
+
+		cfg := config.Config{
+			General: config.GeneralConfig{
+				DBType: core.MongoDB,
+			},
+		}
+		extCfg := config.ExternalConfig{
+			MongoDB: config.MongoDBConfig{
+				URI:                   inMemoryMongoDB.URI(),
+				DBName:                "dbName",
+				ConnectTimeoutInSec:   10,
+				OperationTimeoutInSec: 10,
+				NumUsersCollections:   8,
+			},
+		}
+
+		// instantiate storage multiple times
+		for i := 0; i < 10; i++ {
+			ssf := NewStorageWithIndexFactory(cfg, extCfg)
+			assert.False(t, check.IfNil(ssf))
+			shardedStorageInstance, err := ssf.Create()
+			assert.Nil(t, err)
+			assert.False(t, check.IfNil(shardedStorageInstance))
+
+			client, err := mongodb.CreateMongoDBClient(ssf.externalCfg.MongoDB)
+			require.Nil(t, err)
+
+			for j := uint32(0); j < extCfg.MongoDB.NumUsersCollections; j++ {
+				key := []byte{byte(j)}
+				err = shardedStorageInstance.Put(key, []byte("data"))
+				require.Nil(t, err)
+				checkCollectionIDsMapping(t, client, key)
+			}
+		}
+	})
+}
+
+func checkCollectionIDsMapping(t *testing.T, client mongodb.MongoDBClient, key []byte) {
+	clientCollections := client.GetAllCollectionsIDs()
+
+	index := int(key[0])
+
+	for i, coll := range clientCollections {
+		if i == int(index) {
+			_, err := client.Get(coll, key)
+			require.Nil(t, err)
+
+			continue
+		}
+
+		_, err := client.Get(coll, key)
+		require.Equal(t, storage.ErrKeyNotFound, err)
+	}
+
+	err := client.Remove(clientCollections[index], key)
+	require.Nil(t, err)
 }
 
 func removeDBs(t *testing.T, cfg config.Config) {
