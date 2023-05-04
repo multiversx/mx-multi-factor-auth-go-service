01319288a420fdc252579187a5b312530a8b8a38
AdoAdoAdo <anaridado@gmail.com>
2023-05-04 16:59:41 +0300

    fixes and simplification

--- core/sync/rwmutex.go
+++ core/sync/rwmutex.go
@@ -3,69 +3,59 @@ package sync
 import "sync"
 
 // rwMutex is a mutex that can be used to lock/unlock a resource
+// this component is not concurrent safe, concurrent accesses need to be managed by the caller
 type rwMutex struct {
-	internalMut sync.RWMutex
-	cntLocks    uint32
-	cntRLocks   uint32
+	cntLocks  int32
+	cntRLocks int32
 
 	controlMut sync.RWMutex
 }
 
-// NewRWMutex returns a new instance of rwMutex
-func NewRWMutex() *rwMutex {
+// newRWMutex returns a new instance of rwMutex
+func newRWMutex() *rwMutex {
 	return &rwMutex{}
 }
 
 func (rm *rwMutex) updateCounterLock() {
-	rm.internalMut.Lock()
 	rm.cntLocks++
-	rm.internalMut.Unlock()
 }
 
 func (rm *rwMutex) updateCounterRLock() {
-	rm.internalMut.Lock()
 	rm.cntRLocks++
-	rm.internalMut.Unlock()
 }
 
 func (rm *rwMutex) updateCounterUnlock() {
-	rm.internalMut.Lock()
 	rm.cntLocks--
-	rm.internalMut.Unlock()
 }
 
 func (rm *rwMutex) updateCounterRUnlock() {
-	rm.internalMut.Lock()
 	rm.cntRLocks--
-	rm.internalMut.Unlock()
 }
 
-// Lock locks the rwMutex
+// lock locks the rwMutex
 func (rm *rwMutex) lock() {
 	rm.controlMut.Lock()
 }
 
-// Unlock unlocks the rwMutex
+// unlock unlocks the rwMutex
 func (rm *rwMutex) unlock() {
 	rm.controlMut.Unlock()
 }
 
-// RLock locks for read the rwMutex
+// rLock locks for read the rwMutex
 func (rm *rwMutex) rLock() {
 	rm.controlMut.RLock()
 }
 
-// RUnlock unlocks for read the rwMutex
+// rUnlock unlocks for read the rwMutex
 func (rm *rwMutex) rUnlock() {
 	rm.controlMut.RUnlock()
 }
 
-// NumLocks returns the number of locks on the rwMutex
-func (rm *rwMutex) numLocks() uint32 {
-	rm.internalMut.RLock()
+// numLocks returns the number of locks on the rwMutex
+func (rm *rwMutex) numLocks() int32 {
 	cntLocks := rm.cntLocks
 	cntRLocks := rm.cntRLocks
-	rm.internalMut.RUnlock()
 
 	return cntLocks + cntRLocks
 }
--- core/sync/rwmutex_test.go
+++ core/sync/rwmutex_test.go
@@ -1,7 +1,6 @@
 package sync
 
 import (
-	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,10 +9,10 @@ import (
 func TestNewRWMutex(t *testing.T) {
 	t.Parallel()
 
-	cs := NewRWMutex()
+	cs := newRWMutex()
 	require.NotNil(t, cs)
-	require.Equal(t, uint32(0), cs.cntLocks)
-	require.Equal(t, uint32(0), cs.cntRLocks)
+	require.Equal(t, int32(0), cs.cntLocks)
+	require.Equal(t, int32(0), cs.cntRLocks)
 }
 
 func TestRWMutex_Lock_Unlock_IsLocked_NumLocks(t *testing.T) {
@@ -22,54 +21,17 @@ func TestRWMutex_Lock_Unlock_IsLocked_NumLocks(t *testing.T) {
 	cs := &rwMutex{}
 	cs.lock()
 	cs.updateCounterLock()
-	require.Equal(t, uint32(1), cs.numLocks())
+	require.Equal(t, int32(1), cs.numLocks())
 
 	cs.unlock()
 	cs.updateCounterUnlock()
-	require.Equal(t, uint32(0), cs.numLocks())
+	require.Equal(t, int32(0), cs.numLocks())
 
 	cs.rLock()
 	cs.updateCounterRLock()
-	require.Equal(t, uint32(1), cs.numLocks())
+	require.Equal(t, int32(1), cs.numLocks())
 
 	cs.rUnlock()
 	cs.updateCounterRUnlock()
-	require.Equal(t, uint32(0), cs.numLocks())
-}
-
-func TestRWMutex_MultipleLocksUnlocks(t *testing.T) {
-	t.Parallel()
-
-	cs := &rwMutex{}
-	numConcurrentCalls := 500
-	wg := sync.WaitGroup{}
-
-	f := func(wg *sync.WaitGroup, cs *rwMutex) {
-		cs.updateCounterLock()
-		cs.lock()
-		_ = cs.numLocks()
-
-		cs.updateCounterUnlock()
-		cs.unlock()
-		_ = cs.numLocks()
-
-		cs.updateCounterRLock()
-		cs.rLock()
-		_ = cs.numLocks()
-
-		cs.updateCounterRUnlock()
-		cs.rUnlock()
-		_ = cs.numLocks()
-
-		wg.Done()
-	}
-
-	wg.Add(numConcurrentCalls)
-
-	for i := 1; i <= numConcurrentCalls; i++ {
-		go f(&wg, cs)
-	}
-	wg.Wait()
-
-	require.Equal(t, uint32(0), cs.numLocks())
+	require.Equal(t, int32(0), cs.numLocks())
 }
