4a8a75833f0298511a19dbb7590ed506bdc1ff7a
ssd04 <dariussuirab@gmail.com>
2023-06-14 15:14:25 +0300

    reference redis client connection + logging

--- redis/redisFactory.go
+++ redis/redisFactory.go
@@ -4,17 +4,18 @@ import (
 	"github.com/go-redis/redis_rate/v10"
 	"github.com/multiversx/multi-factor-auth-go-service/config"
 	"github.com/multiversx/multi-factor-auth-go-service/core"
+	logger "github.com/multiversx/mx-chain-logger-go"
 	"github.com/redis/go-redis/v9"
 )
 
+var log = logger.GetOrCreate("redis")
+
 // CreateRedisRateLimiter will create a new redis rate limiter component
 func CreateRedisRateLimiter(cfg config.RedisConfig, twoFactorCfg config.TwoFactorConfig) (RateLimiter, error) {
-	opt, err := redis.ParseURL(cfg.URL)
+	client, err := createRedisClient(cfg)
 	if err != nil {
 		return nil, err
 	}
-
-	client := redis.NewClient(opt)
 	redisLimiter := redis_rate.NewLimiter(client)
 
 	rateLimiterArgs := ArgsRateLimiter{
@@ -29,17 +30,25 @@ func CreateRedisRateLimiter(cfg config.RedisConfig, twoFactorCfg config.TwoFacto
 func createRedisClient(cfg config.RedisConfig) (*redis.Client, error) {
 	switch cfg.ConnectionType {
 	case core.RedisInstanceConnType:
-		opt, err := redis.ParseURL(cfg.URL)
-		if err != nil {
-			return nil, err
-		}
-
-		return redis.NewClient(opt), nil
+		return createSimpleClient(cfg)
 	case core.RedisSentinelConnType:
 		return createFailoverClient(cfg)
 	default:
 		return nil, core.ErrInvalidRedisConnType
 	}
+}
+
+func createSimpleClient(cfg config.RedisConfig) (*redis.Client, error) {
+	opt, err := redis.ParseURL(cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	client := redis.NewClient(opt)
+
+	log.Debug("created redis instance connection type", "connection url", cfg.URL)
+
+	return client, nil
 }
 
 // createFailoverClient will create a redis client for a redis setup with sentinel
@@ -48,6 +57,8 @@ func createFailoverClient(cfg config.RedisConfig) (*redis.Client, error) {
 		MasterName:    cfg.MasterName,
 		SentinelAddrs: []string{cfg.SentinelUrl},
 	})
+
+	log.Debug("created redis sentinel connection type", "connection url", cfg.SentinelUrl, "master", cfg.MasterName)
 
 	return client, nil
 }
